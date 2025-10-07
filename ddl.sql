38b259e3eac978cf1b159e0708d016f5ec23cfe3
Adrian Ruiz <addsdeveloper@gmail.com>
2025-10-07 11:40:29 -0500

    Refactor database schema: update foreign key references and remove redundant table definitions

--- ddl.sql
+++ ddl.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS `ecommerce_zapatos`;
 CREATE DATABASE IF NOT EXISTS `ecommerce_zapatos`;
 USE `ecommerce_zapatos`;
 
@@ -9,17 +10,15 @@ CREATE TABLE `Usuarios` (
   `correo` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`usuario_id`)
 );
+
 CREATE TABLE `Pedidos` (
   `pedido_id` int NOT NULL AUTO_INCREMENT,
-  `usuario_id` int DEFAULT NULL,
+  `usuario_id_fk` int DEFAULT NULL,
   `fecha_pedido` date DEFAULT NULL,
   `total` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`pedido_id`),
-  KEY `usuario_id` (`usuario_id`),
-  CONSTRAINT `Pedidos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `Usuarios` (`usuario_id`)
+  CONSTRAINT `Pedidos_ibfk_1` FOREIGN KEY (`usuario_id_fk`) REFERENCES `Usuarios` (`usuario_id`)
 );
-
-
 
 CREATE TABLE `Productos`(
     `producto_id` INT AUTO_INCREMENT,
@@ -30,31 +29,10 @@ CREATE TABLE `Productos`(
 );
 
 CREATE TABLE `PedidoProducto`(
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `pedido_id` INT NOT NULL,
-    `producto_id` INT NOT NULL,
+    `pedido_id_fk` INT NOT NULL,
+    `producto_id_fk` INT NOT NULL,
     `cantidad` INT NOT NULL DEFAULT 1,
-    PRIMARY KEY(`id`)
-);
-
-ALTER TABLE `PedidoProducto` ADD FOREIGN KEY(`pedido_id`) REFERENCES `Pedidos`(`pedido_id`);
-
-ALTER TABLE `PedidoProducto` ADD CONSTRAINT `Productos_producto_id_fk` FOREIGN KEY(`producto_id`) REFERENCES `Productos`(`producto_id`);
-
-
-CREATE TABLE `Productos`(
-    `producto_id` INT AUTO_INCREMENT,
-    `nombre` VARCHAR(50) NOT NULL,
-    `precio_unitario` DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-    `precio_venta` DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-    PRIMARY KEY (`producto_id`)
-);
-
-CREATE TABLE `PedidoProductoCompuesta`(
-    `pedido_id` INT NOT NULL,
-    `producto_id` INT NOT NULL,
-    `cantidad` INT NOT NULL DEFAULT 1,
-    PRIMARY KEY(`pedido_id`, `producto_id`),
-    FOREIGN KEY(`pedido_id`) REFERENCES `Pedidos`(`pedido_id`),
-    FOREIGN KEY(`producto_id`) REFERENCES `Productos`(`producto_id`)
+    PRIMARY KEY(`pedido_id_fk`, `producto_id_fk`),
+    FOREIGN KEY(`pedido_id_fk`) REFERENCES `Pedidos`(`pedido_id`),
+    FOREIGN KEY(`producto_id_fk`) REFERENCES `Productos`(`producto_id`)
 );
